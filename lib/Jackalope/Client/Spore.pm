3ac696df56025b7c34c1946f70c3873e7f45baf6
Stevan Little <stevan.little@iinteractive.com>
2011-07-13 15:31:57 -0400

    notes for later

--- lib/Jackalope/Client/Spore.pm
+++ lib/Jackalope/Client/Spore.pm
@@ -7,6 +7,13 @@ use JSON::XS;
 sub discover {
     my ($self, $base_url) = @_;
 
+    # NOTE:
+    # we are assuming the
+    # describedby link
+    # is present. Is that
+    # okay? I dunno.
+    # - SL
+
     my $discovery_client = Net::HTTP::Spore->new_from_string(
         JSON::XS->new->encode({
             base_url => $base_url,
@@ -30,8 +37,19 @@ sub discover {
                     ($_->{'rel'} => {
                         path   => $_->{'href'},
                         method => $_->{'method'},
+                        # FIXME:
+                        # this makes a guess here
+                        # and not really a very
+                        # good one actually.
+                        # - SL
                         (exists $_->{'data_schema'} && ($_->{'method'} eq 'PUT' || $_->{'method'} eq 'POST')
                             ? (header => { 'content-type' => 'application/json' })
+                            # TODO:
+                            # deal with links that have
+                            # GET and data_schema by
+                            # inflating the form-params
+                            # for SPORE
+                            # - SL
                             : ())
                     })
                 } values %{ $schema->{'links'} }
