9c811a552dfe7aa22647105d6b32622074f4383c
Stevan Little <stevan.little@iinteractive.com>
2011-07-13 22:28:43 -0400

    do a better job of handling the linkrel -> method transform

--- lib/Jackalope/Client/Spore.pm
+++ lib/Jackalope/Client/Spore.pm
@@ -18,7 +18,13 @@ sub discover {
         JSON::XS->new->encode({
             base_url => $base_url,
             version  => '0.01',
-            methods  => { describedby => { path => '/', method => 'OPTIONS' } }
+            methods  => {
+                describedby => {
+                    path            => '/',
+                    method          => 'OPTIONS',
+                    expected_status => [ 200 ]
+                }
+            }
         })
     );
     $discovery_client->enable('Format::JSON');
@@ -28,38 +34,73 @@ sub discover {
     my $id     = $data->{'id'};
     my $schema = $data->{'body'}->{ $id };
 
+    my $methods = {};
+    foreach my $linkrel ( values %{ $schema->{'links'} } ) {
+        $methods->{ $linkrel->{'rel'} } = $self->jackalope_linkrel_to_spore_method( $linkrel );
+    }
+
+    use Data::Dumper; warn Dumper $methods;
+
     my $client = Net::HTTP::Spore->new_from_string(
         JSON::XS->new->encode({
             base_url => $base_url,
             version  => '0.01',
-            methods  => {
-                map {
-                    ($_->{'rel'} => {
-                        path   => $_->{'href'},
-                        method => $_->{'method'},
-                        # FIXME:
-                        # this makes a guess here
-                        # and not really a very
-                        # good one actually.
-                        # - SL
-                        (exists $_->{'data_schema'} && ($_->{'method'} eq 'PUT' || $_->{'method'} eq 'POST')
-                            ? (header => { 'content-type' => 'application/json' })
-                            # TODO:
-                            # deal with links that have
-                            # GET and data_schema by
-                            # inflating the form-params
-                            # for SPORE
-                            # - SL
-                            : ())
-                    })
-                } values %{ $schema->{'links'} }
-            }
+            methods  => $methods
         })
     );
     $client->enable('+Jackalope::Client::Spore::Middleware::InflateResource');
     $client->enable('Format::JSON');
 
     $client;
+}
+
+sub jackalope_linkrel_to_spore_method {
+    my ($self, $linkrel) = @_;
+
+    my %additional;
+
+    if ( exists $linkrel->{'data_schema'} ) {
+        my $data_schema = $linkrel->{'data_schema'};
+
+        if ($linkrel->{'method'} eq 'PUT' || $linkrel->{'method'} eq 'POST') {
+            $additional{'header'}           = { 'content-type' => 'application/json' };
+            $additional{'required_payload'} = 1;
+        }
+
+        if ($linkrel->{'method'} eq 'GET') {
+
+            if ( exists $data_schema->{'properties'} ) {
+                $additional{'required_params'} = [
+                    keys %{ $data_schema->{'properties'} }
+                ];
+            }
+
+            if ( exists $data_schema->{'additional_properties'} ) {
+                $additional{'optional_params'} = [
+                    keys %{ $data_schema->{'additional_properties'} }
+                ];
+            }
+
+        }
+    }
+
+    if ( exists $linkrel->{'uri_schema'} ) {
+        my $uri_schema = $linkrel->{'uri_schema'};
+        $additional{'required_params'} = [
+            keys %$uri_schema
+        ];
+    }
+
+    $additional{'expected_status'} = [ 200 ] if $linkrel->{'method'} eq 'GET' || $linkrel->{'method'} eq 'OPTIONS';
+    $additional{'expected_status'} = [ 201 ] if $linkrel->{'method'} eq 'POST';
+    $additional{'expected_status'} = [ 202 ] if $linkrel->{'method'} eq 'PUT';
+    $additional{'expected_status'} = [ 204 ] if $linkrel->{'method'} eq 'DELETE';
+
+    return +{
+        path   => $linkrel->{'href'},
+        method => $linkrel->{'method'},
+        %additional
+    }
 }
 
 
